d3e28ae56c38cb67d63f712d08b9014748100f7c
shiho <gee.kwus@gmail.com>
2020-10-02 00:34:18 +0900

    lalavel_10

--- mynews/routes/web.php
+++ mynews/routes/web.php
@@ -14,3 +14,15 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+//PHP_lalavel_10 課題3
+Route::get('XXX','AAAcontroller@bbb');
+
+
+Route::group(['prefix' => 'admin'], function(){
+  Route::get('news/create','Admin\NewsController@add');
+  //PHP_lalavel_10　課題4
+  Route::get('profile/create','Admin\ProfileController@add');
+  Route::get('profile/edit','Admin\ProfileController@edit');
+
+});
