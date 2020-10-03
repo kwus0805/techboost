4e616049b325d0be480a3a1721bcf76a1cc3e7ac
shiho <gee.kwus@gmail.com>
2020-10-03 21:12:49 +0900

    lalavel_12

--- mynews/resources/views/admin/profile/create.blade.php
+++ mynews/resources/views/admin/profile/create.blade.php
@@ -1,13 +1,16 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+{{-- layouts/profile.blade.phpを読みこむ --}}
+@extends('layouts.profile')
 
-    <title>MyProfile</title>
-  </head>
-  <body>
-    <h1>Myプロフィール</h1>
-  </body>
-</html>
+{{-- profile.blade.phpの@yield('title')に'プロフィールの新規作成'を埋め込む --}}
+@section('title', 'プロフィールの新規作成')
+
+{{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="content">
+      <div class="row">
+        <div class="col-md-8 mx-auto">
+          <h2>プロフィール新規作成</h2>
+        </div>
+      </div>
+    </div>
+@endsection
