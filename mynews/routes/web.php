2810f0641440a4184c3eac9a9f9b4a9f6e632498
shiho <gee.kwus@gmail.com>
2020-10-05 20:51:19 +0900

    課題13

--- mynews/routes/web.php
+++ mynews/routes/web.php
@@ -20,9 +20,13 @@ Route::get('XXX','AAAcontroller@bbb');
 
 
 Route::group(['prefix' => 'admin'], function(){
-  Route::get('news/create','Admin\NewsController@add');
+  Route::get('news/create','Admin\NewsController@add')->middleware('auth');
   //PHP_lalavel_10　課題4
-  Route::get('profile/create','Admin\ProfileController@add');
-  Route::get('profile/edit','Admin\ProfileController@edit');
+  Route::get('profile/create','Admin\ProfileController@add')->middleware('auth');
+  Route::get('profile/edit','Admin\ProfileController@edit')->middleware('auth');
 
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
